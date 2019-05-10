2c3f53565020e0f1de6563c76dfbeced3aecc0d0
Michael Peng <mail@pengmichael.com>
2019-05-09 21:18:05 -0700

    update read handler - forgot to delete dead code

--- src/App/src/Handler/ReadBookingHandler.php
+++ src/App/src/Handler/ReadBookingHandler.php
@@ -22,32 +22,5 @@ class ReadBookingHandler implements RequestHandlerInterface
         $db = (new Database())->getConnection();
         $bookingHelper = new BookingHelper($db);
         return $bookingHelper->readBooking();
-
-        $requestParams = $request->getQueryParams();
-        $requestType = isset($requestParams['handle']) ? $requestParams['handle'] : '';
-        // TODO: split into own Handler files
-        switch ($requestType) {
-            case 'read':
-                return $bookingHelper->readBooking();
-                break;
-            case 'get':
-                return $bookingHelper->getBooking(
-                    isset($requestParams['booking_id']) ? $requestParams['booking_id'] : null
-                );
-                break;
-            case 'create':
-                return $bookingHelper->createBooking(isset($requestParams['data']) ? $requestParams['data'] : []);
-                break;
-            case 'update':
-                return $bookingHelper->updateBooking(
-                    isset($requestParams['booking_id']) ? $requestParams['booking_id'] : null,
-                    isset($requestParams['data']) ? $requestParams['data'] : []
-                );
-                break;
-            default:
-                return new JsonResponse(['error' => 'type not handled']);
-                break;
-        }
     }
-
 }
